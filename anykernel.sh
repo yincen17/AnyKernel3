3fed5e227a8c6807e9cab1a800aa82872e4f1380
Aprianto WPJ <apriantowpj@gmail.com>
2020-10-10 13:41:33 +0700

    Fix typo lmao

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=SilonT kernel by @ThisIsTag
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=joyuse
+device.name1=joyeuse
 device.name2=
 device.name3=
 device.name4=
