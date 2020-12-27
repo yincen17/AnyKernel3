a9ba3289c332b06c004b12bb3b650b5b32e6c237
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-12-27 22:43:06 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SilonT Kernel By @ThisIsTag
+kernel.string=SilonT Kernel By @ThisIsTag and @Yincen
 do.devicecheck=1
 do.modules=0
 do.systemless=1
